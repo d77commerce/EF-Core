b4a2387a7d759cc915edc4a5f84edbda14229032
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-07-01 16:40:19 +0100

    update

---
+++ DB First/DB First/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DB_First
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
