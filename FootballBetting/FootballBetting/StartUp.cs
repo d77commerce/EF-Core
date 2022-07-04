3d0dcd85843266759c1895254254213c68f6bbaf
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-07-04 23:13:31 +0100

    new

---
+++ FootballBetting/FootballBetting/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FootballBetting
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
