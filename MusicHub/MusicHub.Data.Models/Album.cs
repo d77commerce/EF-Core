dd56b1abed7442e1c343aea2793b3eb93c8f66d0
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-07-26 00:25:12 +0100

    update

---
+++ MusicHub/MusicHub.Data.Models/Album.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models
+{
+    public class Album
+    {
+        private decimal _price;
+        public int Id { get; set; }
+
+        [StringLength(40)]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime ReleaseData { get; set; }
+
+
+        public decimal Price
+        {
+            
+            get => _price;
+            set
+            {
+                var collection = new ICollection<Song>();
+            } 
+        }
+    }
+}
--- MusicHub/MusicHub.Data.Models/Song.cs
+++ MusicHub/MusicHub.Data.Models/Song.cs
@@ -1,19 +1,33 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using MusicHub.Data.Models.Resurce;
 
 namespace MusicHub.Data.Models
 {
     public class Song
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public DateTime Duration { get; set; }
-        public DateTime CreateOn { get; set; }
 
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime Duration { get; set; }
+        [Required]
+        public DateTime CreateOn { get; set; }
+        [Required]
         public EnumResurce.Genre Genre { get; set; }
+
         public int AlbumId { get; set; }
 
         public string Album { get; set; }
+        [Required]
+        public int WriterId { get; set; }
+
+        public string Writer { get; set; }
+        [Required]
+        public decimal Price { get; set; }
 
 
     }
